400d077ee4f78614750f40cd0a9fb918114de560
gabrieltoja <gabrieltojaisch@gmail.com>
2022-12-08 01:58:53 -0800

    fix: remove extraneous console.logs, import sf symbols properly

--- Shared (Extension)/Resources/background.js
+++ Shared (Extension)/Resources/background.js
@@ -1,9 +1,6 @@
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request.message);
     if (request.message === "refreshPreferences") {
         browser.runtime.sendNativeMessage("application.id", {message: "refreshPreferences"}, function(response) {
-            console.log("Received sendNativeMessage response:");
-            console.log(response);
             sendResponse(response)
         })
         return true
