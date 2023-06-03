88c88e67f01c8c172cc09a63e0c5e17cc104e761
George Botros <georgebotros02@gmail.com>
2023-06-02 19:40:30 -0700

    test: adds end-to-end & unit testing as well as linting

--- Shared (Extension)/Resources/background.js
+++ Shared (Extension)/Resources/background.js
@@ -1,13 +1,13 @@
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.message === "refreshPreferences") {
-        browser.runtime.sendNativeMessage("application.id", {message: "refreshPreferences"}, function(response) {
-            sendResponse(response)
-        })
-        return true
-    } else if (request.message == "metaOpen") {
-        browser.tabs.create({
-            url: request.options.url,
-            active: false
-        })
-    }
+  if (request.message === 'refreshPreferences') {
+    browser.runtime.sendNativeMessage('application.id', { message: 'refreshPreferences' }, (response) => {
+      sendResponse(response);
+    });
+    return true;
+  } if (request.message == 'metaOpen') {
+    browser.tabs.create({
+      url: request.options.url,
+      active: false,
+    });
+  }
 });
