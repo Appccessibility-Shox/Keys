5b75ffb9e85c03e16b0a1513a5fc58d5e7cdb033
Reiny Botros <reinybo@gmail.com>
2023-06-03 08:15:12 -0700

    chore(lint): edit js files to adhere to airbnb style guide\n\n

--- Shared (Extension)/Resources/background.js
+++ Shared (Extension)/Resources/background.js
@@ -1,10 +1,11 @@
+// eslint-disable-next-line consistent-return
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.message === 'refreshPreferences') {
     browser.runtime.sendNativeMessage('application.id', { message: 'refreshPreferences' }, (response) => {
       sendResponse(response);
     });
     return true;
-  } if (request.message == 'metaOpen') {
+  } if (request.message === 'metaOpen') {
     browser.tabs.create({
       url: request.options.url,
       active: false,
