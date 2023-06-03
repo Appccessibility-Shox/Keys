96c8cb1b1cd8ade5e311015f2051246cd0b66ac1
Patrick Botros <pbotros@amazon.com>
2023-06-03 16:27:05 -0700

    test: adds end-to-end testing for some local & remote pages

--- Shared (Extension)/e2e.test.js
+++ Shared (Extension)/e2e.test.js
@@ -18,7 +18,7 @@ const puppeteerArgs = [
   '--enable-automation',
 ];
 
-describe('Wikipedia tests', () => {
+describe('Local short-keys.html tests', () => {
   let page;
   let browser;
 
@@ -34,7 +34,41 @@ describe('Wikipedia tests', () => {
     page = await browser.newPage();
 
     // navigates to some specific page
-    await page.goto('https://en.wikipedia.org/wiki/Main_Page');
+    await page.goto(`file://${__dirname}/test-pages/short-keys.html`);
+    await page.keyboard.press('g');
+  }, 10000);
+
+
+  afterAll(async () => {
+    // Tear down the browser
+    await browser.close();
+  });
+
+  it('Tests that 1-letter keys can be generated', async () => {
+    const firstLinkKey = await page.evaluate(() => Array.from(document.querySelectorAll('#first-anchor .Keys-Character'), (element) => element.textContent).join(''));
+    const lastLinkKey = await page.evaluate(() => Array.from(document.querySelectorAll('#last-anchor .Keys-Character'), (element) => element.textContent).join(''));
+    expect(firstLinkKey).toBe('A');
+    expect(lastLinkKey).toBe('q');
+  });
+});
+
+describe('Local non-latin-characters.html tests', () => {
+  let page;
+  let browser;
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 250,
+      devtools: true,
+      args: puppeteerArgs,
+    });
+
+    // Creates a new tab
+    page = await browser.newPage();
+
+    // navigates to some specific page
+    await page.goto(`file://${__dirname}/test-pages/non-latin-characters.html`);
     await page.keyboard.press('g');
   }, 10000);
 
@@ -43,10 +77,43 @@ describe('Wikipedia tests', () => {
     await browser.close();
   });
 
-  it('Tests that re is the key for read tab', async () => {
-    // const f = await page.$("[id='ca-view'] .Keys-Character")
-    // obtain text
-    const text = await page.evaluate(() => Array.from(document.querySelectorAll("[id='ca-view'] .Keys-Character"), (element) => element.textContent).join(''));
-    expect(text).toBe('Re');
+  it('Tests that non-latin links are treated like images & non-latin searchboxes get latin placeholders.', async () => {
+    const nonLatinAnchorsThatAreTetherTargets = await page.evaluate(() => Array.from(document.querySelectorAll('#non-latin-anchor.tether-target')).length);
+    expect(nonLatinAnchorsThatAreTetherTargets).toBe(1);
+
+    const nonLatinInputThatAreTetherTargets = await page.evaluate(() => document.querySelector('#non-latin-searchbox').placeholder);
+    expect(nonLatinInputThatAreTetherTargets).toMatch(/[A-Za-z0-9_]/);
   });
+});
+
+describe('Remote youtube.com tests', () => {
+  let page;
+  let browser;
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch({
+      headless: 'new',
+      slowMo: 250,
+      devtools: true,
+      args: puppeteerArgs,
+    });
+
+    // Creates a new tab
+    page = await browser.newPage();
+
+    // navigates to some specific page
+    await page.goto('https://www.youtube.com/');
+    await page.keyboard.press('g');
+  }, 10000);
+
+  afterAll(async () => {
+    // Tear down the browser
+    await browser.close();
+  });
+
+  it('Tests site-specific modification ensuring that home page chips arent emptied by keys', async () => {
+    const chipTextList = await page.$$eval('yt-chip-cloud-chip-renderer', (a) => a.map((e) => e.innerText));
+    // source: https://stackoverflow.com/questions/46377955/puppeteer-page-evaluate-queryselectorall-return-empty-objects
+    expect(chipTextList[0]).not.toBe('');
+  }, 10000);
 });
